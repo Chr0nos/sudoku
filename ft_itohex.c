678d52ce0017981623dcf1d1b46abb7e5843a52a
Chr0nos <hisoka2501@free.fr>
2015-02-26 02:29:18 +0100

    dont't mess with the norm anymore

--- ft_itohex.c
+++ ft_itohex.c
@@ -6,13 +6,14 @@ void ft_itohex(int value, char* s)
     int i;
     int k;
     const char* keys = "0123456789abcdef";
+
     i = 0;
-    k = value % 16;
-    s[i++] = keys[k];
-    while ((value /= 16) > 0)
+    while (value)
     {
+        value /= 16;
         k = value % 16;
-        s[i++] = keys[k];
+        s[i] = keys[k];
+        i++;
     }
     s[i] = '\0';
     my_strrev(s);
