857329a620c68bf5e5b18f1a69a18f87c0bec9c4
Chr0nos <hisoka2501@free.fr>
2015-02-26 13:42:59 +0100

    removed "size" var become useless

--- my_strrev.c
+++ my_strrev.c
@@ -4,12 +4,10 @@ void my_strrev(char *s)
 {
     int i;
     int j;
-    int size;
     char x;
 
-    size = my_strlen(s) -1;
     i = 0;
-    j = size;
+    j = my_strlen(s) -1;
     while (i < j)
     {
         x = s[i];
